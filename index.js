d47845179151f0966b3313a4c8794cfd7d9901c7
Jessica Brumfield <jbrumfield1999@gmail.com>
2018-11-15 21:17:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 // add solution here
-function theBeatlesPlay(musicians, instruments)
-  a= [ ]
-   for (let a=0;a<5;a++){
-     array.push( )
+function theBeatlesPlay(musicians, instruments) {
+  var newArr = []; 
+  for(var i =0; i < musicians.length; i++) {
+    newArr.push(musicians[i] + " plays " + instruments[i]);
+  }
+  return newArr;
+}
+ function johnLennonFacts(){
+   while (true){
+     return `${johnLennonFacts} "!!!"`
    }
+   
+   
+ }
