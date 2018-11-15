eb902635ae49c9ed6a5c5ce728f10e69554a3ff6
Jessica Brumfield <jbrumfield1999@gmail.com>
2018-11-15 21:22:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,16 +6,15 @@ function theBeatlesPlay(musicians, instruments) {
   }
   return newArr;
 }
- function johnLennonFacts() {
-    var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice",];
-    var newFacts = [ ];
-    var i = 0;
-    while (i < facts.length) {
-        newFacts.push(facts[i] +"!!!");
-        i++;
-    }
-    return newFacts;
+ function johnLennonFacts(facts) {
+  const shoutedFacts = []
+   let i = 0
+  while (i < facts.length) {
+    shoutedFacts.push(`${facts[i]}!!!`)
+    i++
 }
+ return shoutedFacts
+ }
  function iLoveTheBeatles(num) {
     var love = [];
     do {
